57db3e1cfc1186000fc54d8b56cbd488845c1529
fernandosouzamarques <fernandinhosouzamarques@gmail.com>
2019-08-16 11:04:33 -0300

    inicio da aplicação

---
+++ back-end/config/database.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+module.exports = function(uri) {
+	mongoose.connect(uri);
+
+	mongoose.connection.on('connected' , () => {
+		console.log('Mongoose! conectado a ' + uri);
+	});
+
+	mongoose.connection.on('disconnected' , () => {
+		console.log('Mongoose! desconectado a ' + uri);
+	});
+
+	mongoose.connection.on('error', (erro) => {
+		console.log('Mongoose! ERROR: ' + erro)
+	})
+
+	// Capturamos um sinal de encerramento (SIGINT), Ctrl+C
+	process.on('SIGINT', () => {
+		mongoose.connection.close(() => {
+			console.log('Mongoose! desconectado pelo término da aplicação');
+			// 0 indica a que a finalização ocorreu sem erros 
+			process.exit(0);
+		});
+	});
+}
